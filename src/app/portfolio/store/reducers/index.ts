085b3cae5f690746474b5a034c684d7ec1334e49
ianacodev <ianaco.dev@gmail.com>
2022-04-20 18:36:11 -0400

    update build

--- src/app/portfolio/store/reducers/index.ts
+++ src/app/portfolio/store/reducers/index.ts
@@ -2,7 +2,7 @@
 import { ActionReducerMap, createFeatureSelector } from '@ngrx/store';
 import * as fromPortfolioCardsReducer from './portfolio-cards.reducer';
 
-export const featureKey = 'products';
+export const featureKey = 'portfolio';
 
 export interface PortfolioState {
   [fromPortfolioCardsReducer.portfolioCardsFeatureKey]: fromPortfolioCardsReducer.PortfolioCardsState;
